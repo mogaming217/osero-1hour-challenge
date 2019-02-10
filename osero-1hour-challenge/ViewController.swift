a17ba87b91d680c7e574111db80d9a7b062a4a17
seiya-mogami <seiya.mogami@dena.com>
2019-02-10 23:10:54 +0900

    Initial Commit

---
+++ osero-1hour-challenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  osero-1hour-challenge
+//
+//  Created by Seiya Mogami on 2019/02/10.
+//  Copyright © 2019 mogaming. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
